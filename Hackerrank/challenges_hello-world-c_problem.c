33525b85db2260a3831f5e8b8a5fae61797da84f
surajit721172 <surajit721172@gmail.com>
2024-03-15 16:33:45 +0530

    hackerrank challenge

---
+++ Hackerrank/challenges_hello-world-c_problem.c
@@ -0,0 +1,7 @@
+// This challenge requires you to print  on a single line, and then print the already provided input string to stdout. If you are not familiar with C, you may want to read about the printf() command.
+#include <stdio.h>
+ int main()
+ {
+     printf("Hello, World!\n Welcome to C programming.");
+     return 0;
+ }
