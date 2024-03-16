135f84abe9de58a6faea426d295dde85c856eb54
surajit721172 <surajit721172@gmail.com>
2024-03-16 22:17:37 +0530

    codechef practice

---
+++ codechef/practice/PCL01_problems_START01.c
@@ -0,0 +1,17 @@
+// Write a program that takes a number 
+
+// N as the input, and prints it to the output.
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	
+	scanf("%d",&n);
+	n=10;
+	
+	printf("%d",n);
+	
+	return 0;
+
+}
