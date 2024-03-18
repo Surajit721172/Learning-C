e533c37f6e458c193bbdf5977d4b0aff096dbcb6
surajit721172 <surajit721172@gmail.com>
2024-03-18 22:09:37 +0530

    print a number

---
+++ codechef/course/LBCL11_problems_CLB054.c
@@ -0,0 +1,15 @@
+// Write a program which does the following
+
+// Declare an integer variable a
+// Try taking a number from the console and assign it to a
+// Output a to the console
+
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+    printf("Your number is: %d", a);
+  
+}
