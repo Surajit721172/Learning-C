aa9fde6459d908c1cd1a60d164b159c94000cae0
surajit721172 <surajit721172@gmail.com>
2024-03-18 20:52:38 +0530

    scan a and b and then print their sum

---
+++ codechef/course/LBCL05_problems_CLB024.c
@@ -0,0 +1,22 @@
+// Write a program which does the following
+
+// Declare two integer variables a and b
+// Assign the value 23 to a and 20 to b
+// Output the sum of a and b to the console
+// To sum a and b, you can print a + b, like we did with numbers previously.
+
+#include <stdio.h>
+
+int main() {
+	// Assign the value 23 to a
+     int a,b;
+     a=23;
+     b=20;
+     printf("%d",23+20);
+     return 0;
+    
+    // Assign the value 20 to b
+
+    
+    // Output the sum of a and b
+}
